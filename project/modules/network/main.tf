cf11ba79694783cd47c2a3ced4014ede485d6a5c
Maryam <20474103+MaryamBerg@users.noreply.github.com>
2025-06-30 14:14:19 +0200

    add changes

--- project/modules/network/main.tf
+++ project/modules/network/main.tf
@@ -9,6 +9,11 @@
 #Association of NAT with Subnet
 #
 #Outputs for subnet_id, vnet_id, and NAT IP
+
+
+
+# todo NSG like firewall
+
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = var.address_space
@@ -31,17 +36,70 @@ resource "azurerm_public_ip" "nat_ip" {
   sku                 = "Standard"
 }
 
+# egress
 resource "azurerm_nat_gateway" "nat" {
   name                = "web-nat-gateway"
   location            = var.location
   resource_group_name = var.resource_group_name
   sku_name            = "Standard"
   idle_timeout_in_minutes = 10
-  public_ip_ids       = [azurerm_public_ip.nat_ip.id]
 }
-
+# egress
+resource "azurerm_nat_gateway_public_ip_association" "example" {
+  nat_gateway_id       = azurerm_nat_gateway.nat.id
+  public_ip_address_id = azurerm_public_ip.nat_ip.id
+}
+# egress
 resource "azurerm_subnet_nat_gateway_association" "nat_assoc" {
   subnet_id      = azurerm_subnet.subnet.id
   nat_gateway_id = azurerm_nat_gateway.nat.id
 }
 
+
+
+resource "azurerm_network_security_group" "web_nsg" {
+  name                = "web-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "Allow-HTTP-Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-SSH-Inbound"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-All-Outbound"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "web_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.web_nsg.id
+}
--- project/modules/vmss/main.tf
+++ project/modules/vmss/main.tf
@@ -11,13 +11,13 @@
 #Outputs for VMSS ID and name
 
 
-resource "azurerm_user_assigned_identity" "this" {
+resource "azurerm_user_assigned_identity" "uai_vmss" {
   name                = var.identity_name
   location            = var.location
   resource_group_name = var.resource_group_name
 }
 
-resource "azurerm_role_assignment" "this" {
+resource "azurerm_role_assignment" "uai_vss_roles" {
   for_each = {
     for idx, role in var.role_assignments :
     idx => role
@@ -25,9 +25,10 @@ resource "azurerm_role_assignment" "this" {
 
   scope                = each.value.scope
   role_definition_name = each.value.role_definition_name
-  principal_id         = azurerm_user_assigned_identity.this.principal_id
+  principal_id         = azurerm_user_assigned_identity.uai_vmss.principal_id
 }
 
+# vm scale set
 resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
   name                = var.vmss_name
   resource_group_name = var.resource_group_name
@@ -65,7 +66,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
 
   identity {
     type         = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.this.id]
+    identity_ids = [azurerm_user_assigned_identity.uai_vmss.id]
   }
 
   tags = {
