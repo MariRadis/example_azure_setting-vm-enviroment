982420598c81ceb9981bead8b73c77249793d11b
Maryam <20474103+MaryamBerg@users.noreply.github.com>
2025-06-30 15:11:54 +0200

    add changes

--- project/apply.sh
+++ project/apply.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Get your current public IP
-MY_IP=$(curl -4 -s ifconfig.me)
+# MY_IP=$(curl -4 -s ifconfig.me)
 
 #terraform init -backend-config="backend.config"
 
-terraform plan -out=somefile.tfplan -var="ssh_source_ip=${MY_IP}/32"
+terraform plan -out=somefile.tfplan #-var="ssh_source_ip=${MY_IP}/32"
 terraform apply somefile.tfplan  #-auto-approve
 
 
-#Are instances healthy?
-#gcloud compute backend-services get-health web-backend --global
+
