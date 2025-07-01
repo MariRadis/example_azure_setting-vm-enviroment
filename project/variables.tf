895060a9c3adb50b5f54822d603ba2ce2ed60f37
Maryam <20474103+MaryamBerg@users.noreply.github.com>
2025-07-01 20:28:59 +0200

    add changes

--- project/variables.tf
+++ project/variables.tf
@@ -18,10 +18,3 @@ variable "subnet_prefix" {
 variable "vm_instance_count" {
   type        = number
 }
-
-
-prefix            = "hasenkamp"
-location          = "West Europe"
-address_space     = ["10.0.0.0/16"]
-subnet_prefix     = ["10.0.1.0/24"]
-vm_instance_count = 2
